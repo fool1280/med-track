3e3fe7492ff36a32153ca51196851242e5f96c95
Anh Nguyen <ng.huyanh.gd@gmail.com>
2023-01-28 23:17:13 -0600

    finish all routes

--- server/server.py
+++ server/server.py
@@ -1,7 +1,8 @@
-from flask import Flask, Response, request
+import re
+from flask import Flask, Response, request, abort
 from flask_pymongo import PyMongo
 from flask_cors import CORS
-from bson import json_util
+from bson import Regex, json_util
 from pymongo import TEXT
 
 app = Flask(__name__)
@@ -10,18 +11,6 @@ mongodb_client = PyMongo(app, uri = "mongodb+srv://anguyen:Thieulam10@cluster0.q
 db = mongodb_client.db
 db["medication"].create_index([("name", TEXT), ("desc", TEXT), ("symptoms", TEXT)])
 
-# Database: medicine database
-# name
-# max__per_day
-# desc
-# symptoms
-
-# Database: record
-# username
-# medicine_name
-# times_of_day
-# stopped_date
-
 @app.route("/")
 def hello_world():
     return "<h1>Home</h1>"
@@ -29,6 +18,11 @@ def hello_world():
 
 @app.route("/insert-a-medicine")
 def insert_a_medicine():
+    # Database: medicine database
+    # name
+    # max__per_day
+    # desc
+    # symptoms = []
     medication = {
         "name": "Ibuprofen",
         "desc": "", 
@@ -49,11 +43,61 @@ def get_all_medicines():
 @app.route("/search-a-medicine")
 def search_a_medicine():
     query = request.args.get('query')
-    medication = db["medication"].find( 
-        {"$text": { "$search": query }} 
-    )
+    if query == "":
+        abort(404)
+    medication = db["medication"].find({"$text": { "$search": query }})
     return Response(
         json_util.dumps(medication),
         mimetype='application/json'
     )
 
+@app.route("/add-medicine", methods = ["POST"])
+def add_medicine():
+    # Database: record
+    # username
+    # medicine
+    # times_of_day
+    # stopped_date: unix timestamp
+
+    data = request.get_json()
+    if "username" in data and "medicine" in data and "times_of_day" in data and "stopped_date" in data:
+        record = {
+            "username": data["username"],
+            "medicine": data["medicine"], 
+            "times_of_day": data["times_of_day"],
+            "stopped_date": data["stopped_date"], 
+        }
+        found = db["medication"].find_one({"name": re.compile('^' + re.escape(data["medicine"]) + '$', re.IGNORECASE)})
+        if found is None:
+            return Response(status=404)
+        name = found["name"]
+        print(name)
+        db["record"].insert_one({
+            "username": data["username"],
+            "medicine": name,
+            "times_of_day": data["times_of_day"],
+            "stopped_date": data["stopped_date"], 
+        })
+        return Response(
+            json_util.dumps(record),
+            mimetype='application/json'
+        )
+    else:
+        return Response(status=404)
+
+@app.route("/get-record")
+def get_record():
+    query = request.args.get('query')
+    if query == "":
+        abort(404)
+
+    found = list(db["record"].find({"username": re.compile('^' + re.escape(query) + '$', re.IGNORECASE)}))
+    if len(found) == 0:
+            return Response(status=404)
+    return Response(
+        json_util.dumps(found),
+        mimetype='application/json'
+    )
+    
+if __name__ == '__main__':
+    app.run(host="localhost", port=8000)
