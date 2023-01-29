76c89fb5fb1468449cde19d3797042d5319b2f1a
Minh Nguyen <minhnguyenxuan60@gmail.com>
2023-01-28 23:24:12 -0600

    add modal

--- med-track/src/HomePage.js
+++ med-track/src/HomePage.js
@@ -4,6 +4,46 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Card from 'react-bootstrap/Card';
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
+function Example() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <Button variant="primary" onClick={handleShow}>
+        Search
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <input/>
+          <div>
+            Woohoo, you're reading this text in a modal!
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
+
+
 function BasicExample() {
   return (
     <div class="page">
@@ -14,8 +54,6 @@ function BasicExample() {
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto">
               <Nav.Link href="#features">Home</Nav.Link>
-              <Nav.Link href="#pricing">Schedule</Nav.Link>
-              <Nav.Link href="#bruh">Pill Finder</Nav.Link>
             </Nav>
             <Nav>
               <Nav.Link href="#deets">Login</Nav.Link>
@@ -36,12 +74,10 @@ function BasicExample() {
             <Card style={{ width: '30rem' }}>
               <Card.Body>
                 <Card.Title>Scheduling</Card.Title>
-                <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
                 <Card.Text>
                   Do you ever forget to take your medication? Our app will track all the medicine you need to take and remind you throughout the day. Feel the satisfaction of doing the right thing, while getting better one pill at a time. Make an account now to get started.
                 </Card.Text>
                 <Card.Link href="#">Card Link</Card.Link>
-                <Card.Link href="#">Another Link</Card.Link>
               </Card.Body>
             </Card>
           </div>
@@ -50,12 +86,10 @@ function BasicExample() {
             <Card style={{ width: '30rem' }}>
               <Card.Body>
                 <Card.Title>Pill Suggestion</Card.Title>
-                <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
                 <Card.Text>
                   Feeling sick and don't know what medication to take? Simply conduct a search with any of your symptoms and get everything you need to know for a speedy recovery. Our database contains a large variety of medications with their pros and cons as well as maximum dosages of each.
                 </Card.Text>
-                <Card.Link href="#"></Card.Link>
-                <Card.Link href="#">Another Link</Card.Link>
+                <Example/>
               </Card.Body>
             </Card>
           </div>
